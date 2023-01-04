a7068a4fe69996c98e3cf3a40a9d105876f7ba22
me-naresh141 <me.naresh141@gmail.com>
2023-01-04 22:13:04 +0530

    first React-Native project

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native'
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}> Hello React Native</Text>
+      <Text style={{fontSize: 30}}> This is my first project</Text>
+    </View>
+  );
+};
+
+export default App;
