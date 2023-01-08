c2c0d97cfcb44a411dc32ba5980e7d0e1b79be93
me-naresh141 <me.naresh141@gmail.com>
2023-01-08 21:01:11 +0530

    create a camponent

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native'
+import {Text, View} from 'react-native';
+import CompanyData from './components/companyData';
+import UserData from './components/userdata';
 const App = () => {
   return (
     <View>
+      <Text style={{fontSize: 30}}> 7th day</Text>
       <Text style={{fontSize: 30}}> Hello React Native</Text>
       <Text style={{fontSize: 30}}> This is my first project</Text>
+      <UserData />
+      <CompanyData />
     </View>
   );
 };
---
+++ components/userdata.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+const UserData = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Name:Naresh</Text>
+      <Text style={{fontSize: 30}}>age:20</Text>
+    </View>
+  );
+};
+
+export default UserData;
