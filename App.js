ef9d3166d4d4634b880c02029a0212b33efba524
me-naresh141 <me.naresh141@gmail.com>
2023-01-10 20:54:52 +0530

    create state

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, Text, View} from 'react-native';
 import CompanyData from './components/companyData';
 import UserData from './components/userdata';
 const App = () => {
+  const [name, updateName] = useState('Naresh state in react native');
+  function testName(){
+    updateName("Naresh practics State")
+  }
   return (
     <View>
       <Text style={{fontSize: 30}}> 7th day</Text>
       <Text style={{fontSize: 30}}> Hello React Native</Text>
       <Text style={{fontSize: 30}}> This is my first project</Text>
+      <Text style={{fontSize: 30}}> {name}</Text>
+      <Button title="Update name" onPress={testName}></Button>
       <UserData />
       <CompanyData />
     </View>
